3c941ff8ed85faaff3c28d3a655885de215d059d
Degreat <lexisyartey@aol.com>
2014-06-24 10:25:22 -0400

    Create max_.py

---
+++ max_.py
@@ -0,0 +1,16 @@
+#exe1: max function
+#this is to return the max if two numbers
+#passed as args
+
+def max_(a, b):
+    if a>=b:
+        return a
+    else:
+        return b
+        
+print max_(5, 7)
+print max_(4, 4)
+print max_(3, 2)
+
+#tests passed
+    
